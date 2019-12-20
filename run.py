f001a1be526b97dc8602ddd0a68dbe4556e96343
jackhsien2002 <jackhsien2002@gmail.com>
2019-12-20 23:28:04 +0800

    refactor run.py

--- run.py
+++ run.py
@@ -1,99 +1,120 @@
 import os
 
-from flask import Flask, render_template, request, session, jsonify
+from flask import Flask, render_template, request, session, jsonify, redirect, url_for
 from flask_session import Session
 from flask_socketio import SocketIO, emit
 from models import *
 from datetime import datetime
-# load dotenv in the base root
 from dotenv import load_dotenv
 from pathlib import Path
 
-load_dotenv(verbose=True)
+app = Flask(__name__)
 
+#import developer key from .env file into environment variable
+load_dotenv(verbose=True)
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
-
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-app = Flask(__name__)
-#if not os.getenv("DATABASE_URL"):
-#    raise RuntimeError("DATABASE_URL is not set")
-
+#raise error message if DATABASE_URL cannot be found in environment variable
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set")
 
 #tell flask sqlAlchemy where that database is
-#app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config['DEBUG'] = True
 Session(app)
 
 #link the database to flask application
 db.init_app(app) 
 
-#app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-app.config['DEBUG'] = True
-
+#point socket io to application
 socketio = SocketIO(app)
-
-temp_messages = []
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    '''
+    return login form to visitor
+    '''
+    return render_template('login.html')
+
+@app.route("/validate_user", methods = ['POST'])
+def validateUser():
+    '''
+    retrieve username from login form and redirect user to channel list if user is validated.
+    @username can be mixture of uppercase, lowercase and 
+    '''
+    #get user name from User model
+    username = request.form.get("username")
+    user = User.getUserByName(username)
+    #if user does not exist in User model
+    if not user:
+        #create a new user
+        user = User.createUser(username)
+    #load user information to Flask session
+    session["username"] = user.username
+    session["user_id"] = user.id
+    #redirect user to list of channel
+    return redirect(url_for('channelList'))
 
 @app.route("/logout")
 def logout():
+    '''
+    if user logout, remove user's session at Flask file system.
+    '''
     session.pop('username', None)
     return render_template('logout.html')
 
 @app.route("/list", methods = ["POST", "GET"])
 def channelList():
+    '''
+    a view that displaye
+    '''
+    #if user want to create channel
     if request.method == "POST":
         if request.form.get("channel_name"):
-            channel_name = request.form.get("channel_name")
+            #get user from User model according to username; if user does not login, return error message
             user = User.getUserByName(session["username"])
             if user == None:
-                error_message = "user has not enter name"
+                error_message = "user has not login"
                 return render_template("error.html", error_message = error_message)
-            user.createChannel(channel_name)            
-        if request.form.get("username"):
-            username = request.form.get("username")
-            user = User.getUserByName(username)
-            if not user:
-                user = User.createUser(username)
-            session["username"] = user.username
-            session["user_id"] = user.id
-
+            #if user login, create the channel according to submitted channel name
+            channel_name = request.form.get("channel_name")
+            user.createChannel(channel_name)
+    #setup a list of channels and username for template rendering
     username = session.get("username")
     channels = Channel.query.all()
     return render_template("channel_list.html", username = username, channels = channels)
 
 @app.route("/channel/<string:channel_name>")
 def channel(channel_name):
-    #user = User.getUserByName(session["username"])
-    #display last 5 messages
+    '''
+    show all the messages that are in corresponding channel
+    
+    need to store channel id in session. Whenever user want to post a message, 
+    message will be saved at right channel according to channel id in session
+    '''
     channel = Channel.getChannelByName(channel_name)
-    messages = Message.query.join(User).filter(Message.channel_id == channel.id).order_by(Message.id.desc()).limit(10)
     session["channel_id"] = channel.id
-    #messages = channel.getLastThreeMessages()
     return render_template("channel.html", 
                             channel = channel, 
                             username = session["username"])
 
 @app.route("/post", methods = ["POST"])
 def post():
+    '''
+    When 
+    '''
     start = int(request.form.get("start"))
     amount = int(request.form.get("amount"))
-    print(start, amount)
     channel_id = session['channel_id']
     messages = Message.query.join(User).filter(Message.channel_id == channel_id).order_by(Message.times.desc())[start:start+amount]
-    #using sql query a range of message obj by specifying start and end; return messages
+    #query a range of message by specifying start and end; return messages
     data = []
     for message in messages:
         message_time = message.times.strftime('%I:%M:%S at %m/%d')
@@ -107,30 +128,38 @@ def post():
 
 @socketio.on("send message")
 def sendMessage(data):
+    #setup information that is relevant to message
     user_id = session["user_id"]
     channel_id = session['channel_id']
     message = data["message"]
     username = session["username"]
     now_time = datetime.now(tz = None)
-
+    
+    #write message to database
     m = Message(
             user_id = user_id,
             channel_id = channel_id,
             text = message, 
             times = now_time
         )
-
     db.session.add(m)
     db.session.commit()
-
-    #do something with database
+    #prepare format of time in terms of hour:minute:secoond at month/day
+    message_time = now_time.strftime('%I:%M:%S at %m/%d')
+    
     #broadcast it to all the browsers
-    message_time = datetime.now(tz = None).strftime('%M:%S at %m/%d')
-    print(m.user.username, m.text, message_time)
-    emit("broadcast message",
-        {"message" : m.text, "username" : m.user.username, "times" : message_time, "is_sent" : True},
-        broadcast = True)
+    emit(
+        "broadcast message",
+        {
+            "message" : m.text, 
+            "username" : m.user.username, 
+            "times" : message_time, 
+            "is_sent" : True
+        },
+        broadcast = True
+    )
 
 
 if __name__ == '__main__':
+    #if this file is executed by python interpreter, run the socket io along with application
     socketio.run(app)
